d7e50047751344920a56aea6239b484055b448e8
Lesz <codewithafar@gmail.com>
2026-01-18 15:06:29 +0800

    Enhance scroll smoothness with increased scrub value

--- src/components/sections/RoleShowcase.jsx
+++ src/components/sections/RoleShowcase.jsx
@@ -26,7 +26,7 @@ const RoleShowcase = () => {
         trigger: containerRef.current,
         start: "top top",
         end: "+=300%", // Reduced to 300% to aggressively fix "scrolling thrice"
-        scrub: 0.5,    // Reduced from 1.5 for snappier response
+        scrub: 2.5,    // Increased to 2.5 for Awwwards-style smooth damping
         pin: true,
       }
     });
